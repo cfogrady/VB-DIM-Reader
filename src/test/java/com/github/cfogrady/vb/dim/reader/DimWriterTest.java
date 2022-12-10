0afe5d953368a98e5074ba440e309cc366874774
Christopher Milne-O'Grady <cfogrady@gmail.com>
2022-12-09 23:10:16 -0500

    Remove obsolete test.

--- src/test/java/com/github/cfogrady/vb/dim/reader/DimWriterTest.java
+++ src/test/java/com/github/cfogrady/vb/dim/reader/DimWriterTest.java
@@ -9,22 +9,6 @@ import org.junit.jupiter.api.Test;
 import java.io.*;
 
 public class DimWriterTest {
-    @Test
-    void testThatDimReaderWorks() throws IOException {
-        DimReader reader = new DimReader();
-        File file = new File("C:\\dev\\Digimon Hacking\\01._Agumon_EDBEEEBB.bin");
-        InputStream fileInputStream = new FileInputStream(file);
-        DimContent content = reader.readDimData(fileInputStream, false);
-        fileInputStream.close();
-
-        DimWriter writer = new DimWriter();
-        file = new File("testDIM.bin");
-        OutputStream outputStream = new FileOutputStream(file, false);
-        writer.writeDimData(content, outputStream);
-        outputStream.flush();
-        outputStream.close();
-
-    }
 
     @Test
     void testThatDimWriterWritesIdenticalForNormalDIM() throws IOException {
