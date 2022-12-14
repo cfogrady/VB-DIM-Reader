0f50c751f88114d0e091c0a17b4aad5204bd362a
Christopher Milne-O'Grady <cfogrady@gmail.com>
2022-12-13 23:43:07 -0500

    Granular testing to be done

---
+++ src/test/java/com/github/cfogrady/vb/dim/writer/SpriteChecksumHackerTest.java
@@ -0,0 +1,33 @@
+package com.github.cfogrady.vb.dim.writer;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SpriteChecksumHackerTest {
+
+    @Test
+    void testThatSpritesStayWhenChecksumMatches() {
+        //fully encompass, and split between sprites, multiple split between sprites
+        Assertions.assertTrue(false, "Write the test");
+    }
+
+    @Test
+    void testThatEncompassingSpriteIsMovedToChecksumStartWhenChecksumMatchFails() {
+        Assertions.assertTrue(false, "Write the test");
+    }
+
+    @Test
+    void testThatSpritesSplitHasChecksumBetweenThem() {
+        Assertions.assertTrue(false, "Write the test");
+    }
+
+    @Test
+    void testThatMultipleSpritesSplitHaveChecksumBeforeLast() {
+        Assertions.assertTrue(false, "Write the test");
+    }
+
+    @Test
+    void testThatAWeaveMatchesChecksumsAfter() {
+        Assertions.assertTrue(false, "Write the test");
+    }
+}
