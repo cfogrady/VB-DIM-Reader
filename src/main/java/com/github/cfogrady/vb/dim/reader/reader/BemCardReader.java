c239d459301caafa6fbdf7bf4eb653afeebd1c64
Christopher Milne-O'Grady <cfogrady@gmail.com>
2022-12-18 23:11:57 -0500

    Get DIMReader setup to read either card type.

---
+++ src/main/java/com/github/cfogrady/vb/dim/reader/reader/BemCardReader.java
@@ -0,0 +1,9 @@
+package com.github.cfogrady.vb.dim.reader.reader;
+
+import com.github.cfogrady.vb.dim.reader.content.bem.BemContent;
+
+public class BemCardReader {
+    public BemContent readBemCard(byte[] headerBytes, InputStreamWithNot inputStream) {
+        return null;
+    }
+}
