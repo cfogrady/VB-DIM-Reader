f4dfd74192e384507144a1a3d1c907462850b919
Christopher Milne-O'Grady <cfogrady@gmail.com>
2022-12-19 21:49:48 -0500

    Use relative output stream for header

--- src/main/java/com/github/cfogrady/vb/dim/header/BemHeaderWriter.java
+++ src/main/java/com/github/cfogrady/vb/dim/header/BemHeaderWriter.java
@@ -2,6 +2,7 @@ package com.github.cfogrady.vb.dim.header;
 
 import com.github.cfogrady.vb.dim.util.ByteOffsetOutputStream;
 import com.github.cfogrady.vb.dim.util.ByteUtils;
+import com.github.cfogrady.vb.dim.util.RelativeByteOffsetOutputStream;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
@@ -9,20 +10,21 @@ import java.io.IOException;
 @Slf4j
 public class BemHeaderWriter {
     public void writeHeader(BemHeader headerData, ByteOffsetOutputStream outputStream) throws IOException {
-        outputStream.writeZerosUntilOffset(0x10);
+        RelativeByteOffsetOutputStream relativeOutputStream = new RelativeByteOffsetOutputStream(outputStream);
+        relativeOutputStream.writeZerosUntilOffset(0x10);
         log.info("Text: {}", headerData.getText());
-        outputStream.writeBytes(headerData.getText().getBytes());
-        outputStream.writeZerosUntilOffset(0x32);
-        outputStream.writeBytes(ByteUtils.convert16BitIntToBytes(headerData.getDimId()));
-        outputStream.writeBytes(ByteUtils.convert16BitIntToBytes(headerData.getDimId()));
-        outputStream.writeBytes(ByteUtils.convert16BitIntToBytes(headerData.getProductionYear()));
-        outputStream.writeBytes(ByteUtils.convert16BitIntToBytes(headerData.getProductionMonth()));
-        outputStream.writeBytes(ByteUtils.convert16BitIntToBytes(headerData.getProductionDay()));
-        outputStream.writeBytes(ByteUtils.convert16BitIntToBytes(headerData.getRevisionNumber()));
-        outputStream.writeBytes(ByteUtils.convert16BitIntToBytes(0));
-        outputStream.writeBytes(headerData.getHeaderSignature());
-        outputStream.writeZerosUntilOffset(0x1000);
-        outputStream.writeBytes(headerData.getBemFlags());
-        outputStream.writeBytes(headerData.getSpriteSignature());
+        relativeOutputStream.writeBytes(headerData.getText().getBytes());
+        relativeOutputStream.writeZerosUntilOffset(0x32);
+        relativeOutputStream.writeBytes(ByteUtils.convert16BitIntToBytes(headerData.getDimId()));
+        relativeOutputStream.writeBytes(ByteUtils.convert16BitIntToBytes(headerData.getDimId()));
+        relativeOutputStream.writeBytes(ByteUtils.convert16BitIntToBytes(headerData.getProductionYear()));
+        relativeOutputStream.writeBytes(ByteUtils.convert16BitIntToBytes(headerData.getProductionMonth()));
+        relativeOutputStream.writeBytes(ByteUtils.convert16BitIntToBytes(headerData.getProductionDay()));
+        relativeOutputStream.writeBytes(ByteUtils.convert16BitIntToBytes(headerData.getRevisionNumber()));
+        relativeOutputStream.writeBytes(ByteUtils.convert16BitIntToBytes(0));
+        relativeOutputStream.writeBytes(headerData.getHeaderSignature());
+        relativeOutputStream.writeZerosUntilOffset(0x1000);
+        relativeOutputStream.writeBytes(headerData.getBemFlags());
+        relativeOutputStream.writeBytes(headerData.getSpriteSignature());
     }
 }
