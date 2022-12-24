aaf01ea5e8ae6dca4dd81b820d76fb8126815ec3
Christopher Milne-O'Grady <cfogrady@gmail.com>
2022-12-23 22:53:28 -0500

    Begin implementation to minimize wasted space only to checksum offset

---
+++ src/main/java/com/github/cfogrady/vb/dim/sprite/SpriteWithIndex.java
@@ -0,0 +1,15 @@
+package com.github.cfogrady.vb.dim.sprite;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder(toBuilder = true)
+public class SpriteWithIndex {
+    private final SpriteData.Sprite sprite;
+    private final int index;
+
+    public int getSize() {
+        return sprite.getByteCountAt16BitPerPixel();
+    }
+}
