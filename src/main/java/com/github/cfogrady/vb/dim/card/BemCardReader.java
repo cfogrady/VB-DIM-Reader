3640f595d91d4562899cd97520d5df9c6f201681
Christopher Milne-O'Grady <cfogrady@gmail.com>
2023-04-17 21:31:09 -0400

    Add extra logging to diagnose timing issues.

--- src/main/java/com/github/cfogrady/vb/dim/card/BemCardReader.java
+++ src/main/java/com/github/cfogrady/vb/dim/card/BemCardReader.java
@@ -49,6 +49,7 @@ public class BemCardReader {
         DIMChecksumBuilder checksumBuilder = new DIMChecksumBuilder();
         InputStreamWithNot inputStreamWithNot = new InputStreamWithNot(inputStream, checksumBuilder);
         try {
+            log.info("Read Header Data: {}" + System.currentTimeMillis());
             byte[] headerBytes = inputStreamWithNot.readNBytes(0x1030);
             return readBemCard(headerBytes, inputStreamWithNot);
         } catch (IOException ioe) {
@@ -57,23 +58,40 @@ public class BemCardReader {
     }
 
      BemCard readBemCard(byte[] headerBytes, InputStreamWithNot inputStream) {
+        log.info("Parsing header from bytes at: {}", System.currentTimeMillis());
         BemHeader header = bemHeaderReader.readBemHeaderFromHeaderBytes(headerBytes);
+         log.info("Header Read At: {}. Read to character section", System.currentTimeMillis());
         try {
             inputStream.readToOffset(BemCardConstants.CHARACTER_SECTION_START);
+            log.info("Ready for character section: {}. Reading...", System.currentTimeMillis());
             BemCharacterStats characters = bemCharacterReader.readCharacterStats(inputStream);
+            log.info("Character section loaded: {}. Skip to transformation", System.currentTimeMillis());
             inputStream.readToOffset(BemCardConstants.TRANSFORMATION_SECTION_START);
+            log.info("Ready for transformation section: {}. Reading...", System.currentTimeMillis());
             BemTransformationRequirements transformations = bemTransformationReader.readTransformations(inputStream);
+            log.info("Transformations loaded: {}. Skipping to adventure", System.currentTimeMillis());
             inputStream.readToOffset(BemCardConstants.ADVENTURE_SECTION_START);
+            log.info("Ready for adventures: {}. Reading...", System.currentTimeMillis());
             BemAdventureLevels adventureLevels = bemAdventuresReader.readAdventures(inputStream);
+            log.info("Adventures loaded: {}. Skipping to sprite dimensions", System.currentTimeMillis());
             inputStream.readToOffset(BemCardConstants.SPRITE_DIMENSIONS_START);
+            log.info("Ready for sprite dimensions: {}. Reading", System.currentTimeMillis());
             List<SpriteData.SpriteDimensions> spriteDimensions = bemSpriteReader.readSpriteDimensions(inputStream);
+            log.info("Sprite dimensions loaded: {}. Skipping to attribute fusions", System.currentTimeMillis());
             inputStream.readToOffset(BemCardConstants.ATTRIBUTE_FUSION_START);
+            log.info("Ready for attribute fusions: {}. Reading", System.currentTimeMillis());
             AttributeFusions attributeFusions = bemFusionReader.readAttributeFusion(inputStream);
+            log.info("Attribute fusions loaded: {}. Skipping to specific fusion", System.currentTimeMillis());
             inputStream.readToOffset(BemCardConstants.SPECIFIC_FUSION_START);
+            log.info("Ready for specific fusion: {}. Reading", System.currentTimeMillis());
             BemSpecificFusions specificFusions = bemFusionReader.readSpecificFusions(inputStream);
+            log.info("Specific Fusions loaded: {}. Skipping to sprite data", System.currentTimeMillis());
             inputStream.readToOffset(BemCardConstants.SPRITE_PACKAGE_START);
+            log.info("Ready for Sprite Data: {}. Reading", System.currentTimeMillis());
             SpriteData spriteData = bemSpriteReader.getSpriteData(inputStream, spriteDimensions);
+            log.info("Sprite data loaded: {}. Read to the checksum", System.currentTimeMillis());
             int checksumOnCard = readToChecksum(inputStream);
+            log.info("Checksum read: {}. Building card.", System.currentTimeMillis());
             int calculatedChecksum = inputStream.getChecksum();
             if(checksumOnCard != calculatedChecksum) {
                 log.warn("Checksums don't match! Calculated: {} Received: {}", Integer.toHexString(calculatedChecksum), Integer.toHexString(checksumOnCard));
