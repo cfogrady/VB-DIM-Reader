066958bfbbb9e7789085cc847474034191d0b974
Christopher Milne-O'Grady <cfogrady@gmail.com>
2023-10-19 22:53:46 -0400

    Fix loss of checksum preservation for fewer sprites

--- src/main/java/com/github/cfogrady/vb/dim/sprite/SpriteDimentionsWriter.java
+++ src/main/java/com/github/cfogrady/vb/dim/sprite/SpriteDimentionsWriter.java
@@ -1,5 +1,6 @@
 package com.github.cfogrady.vb.dim.sprite;
 
+import com.github.cfogrady.vb.dim.util.ByteOffsetOutputStream;
 import com.github.cfogrady.vb.dim.util.ByteUtils;
 import com.github.cfogrady.vb.dim.sprite.SpriteData;
 import com.github.cfogrady.vb.dim.util.OutputStreamWithNot;
@@ -7,8 +8,12 @@ import com.github.cfogrady.vb.dim.util.OutputStreamWithNot;
 import java.io.IOException;
 
 public class SpriteDimentionsWriter {
-    public static void writeSpriteDimensions(SpriteData spriteData, OutputStreamWithNot outputStreamWithNot) throws IOException {
+    public static void writeSpriteDimensions(SpriteData spriteData, ByteOffsetOutputStream outputStreamWithNot) throws IOException {
         outputStreamWithNot.writeZerosUntilOffset(0x60000);
+        writeSpriteDimensionsAtCurrentLocation(spriteData, outputStreamWithNot);
+    }
+
+    static void writeSpriteDimensionsAtCurrentLocation(SpriteData spriteData, ByteOffsetOutputStream outputStreamWithNot) throws IOException {
         for(SpriteData.Sprite sprite : spriteData.getSprites()) {
             outputStreamWithNot.writeBytes(ByteUtils.convert16BitIntToBytes(sprite.getWidth()));
             outputStreamWithNot.writeBytes(ByteUtils.convert16BitIntToBytes(sprite.getHeight()));
